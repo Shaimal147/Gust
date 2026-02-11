24b491245fb859973c7aa05b268bdeac531b95f0
Shaimal <shaimalabdulla35@gmail.com>
2026-02-11 22:33:58 +0500

    Create static layout for mobile UI

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,116 @@
+function HomePage() {
+    
+    return (
+        <>
+            <div className="container">
+                <div className='row'>
+                    <div className='col-6'>
+                        <div className='bg-success py-3'>Logo</div>
+                    </div>
+                    <div className='col-6'>
+                        <div className='bg-success py-3 text-end'>Measurement</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container mt-3">
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='bg-success py-5'>GENERIC MESSAGE</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container mt-3'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='bg-success'>Search</div>
+                    </div>
+                </div>
+                <div className='row mt-3'>
+                    <div className='col-12'>
+                        <div className='bg-success'>Search button</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container mt-3">
+                <div className='row g-4'>
+                    <div className='col-6'>
+                        <div className='bg-success p-3'>detail1</div>
+                    </div>
+                    <div className='col-6'>
+                        <div className='bg-success p-3'>detail2</div>
+                    </div>
+                    <div className='col-6'>
+                        <div className='bg-success p-3'>detail3</div>
+                    </div>
+                    <div className='col-6'>
+                        <div className='bg-success p-3'>detail4</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container mt-3'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='bg-success'>daily forecast</div>
+                    </div>
+                </div>
+                <div className='row mt-3'>
+                    <div className='col-4'>
+                        <div className='bg-success p-3'>day1</div>
+                    </div>
+                    <div className='col-4'>
+                        <div className='bg-success p-3'>day2</div>
+                    </div>
+                    <div className='col-4'>
+                        <div className='bg-success p-3'>day3</div>
+                    </div>
+                </div>
+                <div className='row mt-3'>
+                    <div className='col-4'>
+                        <div className='bg-success p-3'>day4</div>
+                    </div>
+                    <div className='col-4'>
+                        <div className='bg-success p-3'>day5</div>
+                    </div>
+                    <div className='col-4'>
+                        <div className='bg-success p-3'>day6</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container mt-3'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='bg-success p-3'>hourly forecast</div>
+                    </div>
+                     <div className='col-12'>
+                        <div className='bg-success p-3 mt-3'>hour1</div>
+                    </div>
+                    <div className='col-12'>
+                        <div className='bg-success p-3 mt-3'>hour2</div>
+                    </div>
+                    <div className='col-12'>
+                        <div className='bg-success p-3 mt-3'>hour3</div>
+                    </div>
+                    <div className='col-12'>
+                        <div className='bg-success p-3 mt-3'>hour4</div>
+                    </div>
+                    <div className='col-12'>
+                        <div className='bg-success p-3 mt-3'>hour5</div>
+                    </div>
+                    <div className='col-12'>
+                        <div className='bg-success p-3 mt-3'>hour6</div>
+                    </div>
+                    <div className='col-12'>
+                        <div className='bg-success p-3 mt-3'>hour7</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
